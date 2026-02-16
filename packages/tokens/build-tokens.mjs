ea3670b34013ea349dfa31d8c242ac974062a052
Tuan N. Truong <tuan.truong625@gmail.com>
2026-02-16 16:21:02 -0500

    Create design tokens

---
+++ packages/tokens/build-tokens.mjs
@@ -0,0 +1,37 @@
+import StyleDictionary from 'style-dictionary';
+
+const sd = new StyleDictionary({
+  usesDtcg: true,
+  source: ['colors.json', 'spacing.json', 'typography.json', 'elevation.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true
+          }
+        }
+      ]
+    },
+    js: {
+      transformGroup: 'js',
+      buildPath: 'build/js/',
+      files: [
+        {
+          destination: 'tokens.js',
+          format: 'javascript/es6'
+        },
+        {
+          destination: 'tokens.d.ts',
+          format: 'typescript/es6-declarations'
+        }
+      ]
+    }
+  }
+});
+
+await sd.buildAllPlatforms();
